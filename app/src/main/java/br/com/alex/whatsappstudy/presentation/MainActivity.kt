a7d5a4f5dff8536570cf935dbe2d4964870405d6
alexSantif <alexsantos2102@gmail.com>
2023-11-08 21:55:53 -0300

    inclusao de tablayout e viewpager

--- app/src/main/java/br/com/alex/whatsappstudy/presentation/MainActivity.kt
+++ app/src/main/java/br/com/alex/whatsappstudy/presentation/MainActivity.kt
@@ -1,12 +1,33 @@
 package br.com.alex.whatsappstudy.presentation
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 import br.com.alex.whatsappstudy.R
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tabLayout = findViewById(R.id.tl_tabs)
+        viewPager = findViewById(R.id.vp_fragments)
+
+        viewPager.adapter = PagerAdapter(this)
+        TabLayoutMediator(tabLayout, viewPager) { tab, index ->
+            tab.text = when(index) {
+                0 -> "Conversas"
+                1 -> "Atualizações"
+                2 -> "Chamadas"
+                else -> { throw Resources.NotFoundException("fragment errado") }
+            }
+        }.attach()
     }
 }
---
+++ app/src/main/java/br/com/alex/whatsappstudy/presentation/PagerAdapter.kt
@@ -0,0 +1,25 @@
+package br.com.alex.whatsappstudy.presentation
+
+import android.content.res.Resources.NotFoundException
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import br.com.alex.whatsappstudy.presentation.calls.CallsFragment
+import br.com.alex.whatsappstudy.presentation.chats.ChatsFragment
+import br.com.alex.whatsappstudy.presentation.status.StatusFragment
+
+class PagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position) {
+            0 -> ChatsFragment()
+            1 -> StatusFragment()
+            2 -> CallsFragment()
+            else -> { throw NotFoundException("fragment errado") }
+        }
+    }
+}
